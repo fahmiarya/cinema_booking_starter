c572584569685d7036f8e83ff8ae5ce9dba5064f
M. Fahmi Arya Maulana <fhazuto@gmail.com>
2026-09-03 04:05:07 +0700

    implemented redis, build web API, Listing seats endpoint

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,46 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
 
+	"github.com/sikozonpc/cinema/internal/adapters/redis"
+	"github.com/sikozonpc/cinema/internal/booking"
+	"github.com/sikozonpc/cinema/internal/utils"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /movies", listMovies)
+
+	mux.Handle("GET /", http.FileServer(http.Dir("static")))
+
+	store := booking.NewRedisStore(redis.NewClient("localhost:6379"))
+	svc := booking.NewService(store)
+
+	bookingHandler := booking.NewHandler(svc)
+
+	mux.HandleFunc("GET /movies/{movieID}/seats", bookingHandler.ListSeats)
+	mux.HandleFunc("POST /movies/{movieID}/seats/{seatID}/hold", bookingHandler.HoldSeat)
+
+	if err := http.ListenAndServe(":8080", mux); err != nil {
+		log.Fatal(err)
+	}
+}
+
+var movies = []movieResponse{
+	{ID: "Interstellar", Title: "Interstellar", Rows: 5, SeatsPerRow: 10},
+	{ID: "HailMary", Title: "Hail Mary", Rows: 5, SeatsPerRow: 10},
+}
+
+func listMovies(w http.ResponseWriter, r *http.Request) {
+	utils.WriteJSON(w, http.StatusOK, movies)
+}
+
+type movieResponse struct {
+	ID          string `json:"id"`
+	Title       string `json:"title"`
+	Rows        int    `json:"rows"`
+	SeatsPerRow int    `json:"seats_per_row"`
 }
