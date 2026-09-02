ff42ac672f4c8b524f2777a3b2e262b968d74b5a
goater <fhazuto@gmail.com>
2026-09-02 22:46:15 +0700

    Set

--- internal/booking/domain.go
+++ internal/booking/domain.go
@@ -1,5 +1,11 @@
 package booking
 
+import "errors"
+var (
+	ErrSeatAlreadyBooked = errors.New("seat is already booked")
+)
+
+
 // Booking represents a confirmed seat reservation.
 type Booking struct {
 	ID      string
